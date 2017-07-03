554fda1858659abe5a09edb954e8178a82d885c7
Gabriele Falace <gabriele.falace@asideas.de>
2017-07-03 14:44:01 +0200

    created root mapping

--- src/main/kotlin/com/teambuilding/moolo/Controller.kt
+++ src/main/kotlin/com/teambuilding/moolo/Controller.kt
@@ -6,17 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/moolo")
 class Controller {
 
     @Autowired lateinit var repo: CommentRepository
 
-    @GetMapping("/hi")
+    @GetMapping("/")
     fun foo(): String {
         repo.save(Comment("123", "Aldo"))
         val comment = repo.findByName("Aldo")
         println(comment)
-
         return "hi"
     }
 }
